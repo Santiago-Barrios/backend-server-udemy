54c64dccc9895236e6db32bbae991cf2ba4c4e19
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-04-23 22:07:10 -0500

    Creando el modelo de usuarios en Node

---
+++ models/usuario.js
@@ -0,0 +1,18 @@
+var moongose = require('mongoose');
+
+var Schema = moongose.Schema;
+
+var usuarioShema = new Schema({
+
+
+    nombre: { type: String, required: [true, 'El nombre es necesario'] },
+    email: { type: String, unique: true, required: [true, 'El correo es necesario'] },
+    passworld: { type: String, required: [true, 'la contraseña es necesaria'] },
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE' },
+
+
+});
+
+
+module.exports = moongose.model('Usuario', usuarioShema);
