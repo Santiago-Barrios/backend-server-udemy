afba6a7b0827eacc696858eda8758c7c95604a1b
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-04-28 19:31:46 -0500

    CRUD medico

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
 
 
 
@@ -38,6 +39,7 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlP
 app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
 app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
 app.use('/', appRoutes);
 
 //escuchar petición
---
+++ routes/medico.js
@@ -0,0 +1,156 @@
+/*jslint  esversion: 6 */
+
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var mdAutebtificacion = require('../middlewares/autentificacion');
+
+var app = express();
+
+var Medico = require('../models/medico');
+
+
+
+//==================================================================
+//Obtener todos los medicos
+//==================================================================
+app.get('/', (request, response, next) => {
+
+
+    Medico.find({}, (err, medicos) => {
+
+        if (err) {
+            return response.status(500).json({
+                ok: false,
+                mensaje: 'Error cargando medicos',
+                errors: err
+            });
+        }
+        response.status(200).json({
+            ok: true,
+            medicos: medicos,
+        });
+
+    });
+
+});
+
+//===============================================================================
+// Actualizar medico
+//===============================================================================
+app.put('/:id', mdAutebtificacion.verificaToken, (request, response) => {
+
+    var id = request.params.id;
+    var body = request.body;
+
+    Medico.findById(id, (err, medico) => {
+
+        if (err) {
+            return response.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar medico',
+                errors: err
+            });
+        }
+
+        if (!medico) {
+
+            return response.status(400).json({
+                ok: false,
+                mensaje: 'Elmedico con el id ' + id + ' no existe',
+                errors: { message: 'No existe un medico con ese ID' }
+            });
+        }
+
+        medico.nombre = body.nombre;
+        medico.usuario = request.usuario._id;
+        medico.hospital = body.hospital;
+
+        medico.save((err, medicoGuardado) => {
+
+            if (err) {
+                return response.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar medico',
+                    errors: err
+                });
+            }
+
+            response.status(200).json({
+                ok: true,
+                medico: medicoGuardado,
+            });
+
+        });
+
+    });
+
+});
+
+//===============================================================================
+// Crear un nuevo medico
+//===============================================================================
+app.post('/', mdAutebtificacion.verificaToken, (request, response) => {
+
+    var body = request.body;
+    var medico = new Medico({
+
+        nombre: body.nombre,
+        usuario: request.usuario._id,
+        hospital: body.hospital,
+
+    });
+
+    medico.save((err, medicoGuardado) => {
+
+        if (err) {
+            return response.status(400).json({
+                ok: false,
+                mensaje: 'Error al crear medico',
+                errors: err
+            });
+        }
+        response.status(201).json({
+            ok: true,
+            medico: medicoGuardado,
+        });
+    });
+
+});
+
+//===============================================================================
+// Borrar un Medico por el id
+//===============================================================================
+app.delete('/:id', mdAutebtificacion.verificaToken, (request, response) => {
+
+    var id = request.params.id;
+
+    Medico.findByIdAndRemove(id, (err, medicoDelete) => {
+
+        if (err) {
+            return response.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar Medico',
+                errors: err
+            });
+        }
+
+        if (!medicoDelete) {
+            return response.status(400).json({
+                ok: false,
+                mensaje: 'No existe un medico con ese ID',
+                errors: { message: 'El medico ha sido borrado o el ID no existe' },
+            });
+        }
+
+        response.status(200).json({
+            ok: true,
+            medico: medicoDelete,
+        });
+    });
+
+
+});
+
+module.exports = app;
