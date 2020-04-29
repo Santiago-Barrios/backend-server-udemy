52f09ba21e3e04054867b5078e8a127cdb2abcc0
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-04-29 17:47:40 -0500

    busqueda general de las colecciones de forma simultanea

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -5,23 +5,26 @@ var express = require('express');
 var app = express();
 
 var Hospital = require('../models/hospital');
-
+var Medico = require('../models/medico');
+var Usuario = require('../models/usuario');
 
 app.get('/todo/:busqueda', (request, response, next) => {
 
     var busqueda = request.params.busqueda;
     var regexp = RegExp(busqueda, 'i');
 
-    buscarHospitales(busqueda, regexp)
-        .then(hospitales => {
+    Promise.all(
+            [buscarHospitales(busqueda, regexp), buscarMedicos(busqueda, regexp), buscarUsuario(busqueda, regexp)])
+        .then(respuestas => {
 
             response.status(200).json({
                 ok: true,
-                hospitales: hospitales,
+                hospitales: respuestas[0],
+                medicos: respuestas[1],
+                usuarios: respuestas[2],
             });
 
         });
-
 
 });
 
@@ -29,23 +32,79 @@ function buscarHospitales(busqueda, regexp) {
 
     return new Promise((resolve, reject) => {
 
-        Hospital.find({ nombre: regexp }, (err, hospitales) => {
+        Hospital.find({ nombre: regexp })
+            .populate('usuario', 'nombre email')
+            .exec(
+                (err, hospitales) => {
 
-            if (err) {
+                    if (err) {
 
-                reject('Error al cargar hospitales ', err);
+                        reject('Error al cargar hospitales ', err);
 
-            } else {
+                    } else {
 
-                resolve(hospitales);
+                        resolve(hospitales);
 
-            }
+                    }
 
 
-        });
+                });
 
     });
 
 }
+
+function buscarMedicos(busqueda, regexp) {
+
+    return new Promise((resolve, reject) => {
+
+        Medico.find({ nombre: regexp })
+            .populate('usuario', 'nombre email')
+            .populate('hospital', 'nombre')
+            .exec(
+                (err, medicos) => {
+
+                    if (err) {
+
+                        reject('Error al cargar medicos ', err);
+
+                    } else {
+
+                        resolve(medicos);
+
+                    }
+
+
+                });
+
+    });
+
+}
+
+function buscarUsuario(busqueda, regexp) {
+
+    return new Promise((resolve, reject) => {
+
+        Usuario.find({}, 'nombre email img role')
+            .or([{ 'nombre': regexp }, { 'email': regexp }])
+            .exec((err, usuarios) => {
+
+                if (err) {
+
+                    reject('Error al buscar usuario ', err);
+
+                } else {
+
+                    resolve(usuarios);
+
+                }
+
+
+            });
+
+    });
+
+}
+
 
 module.exports = app;
