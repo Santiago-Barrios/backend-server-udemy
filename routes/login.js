44a12604e7401e641924515318862fe8ca0da938
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-05-01 18:27:26 -0500

    Validar token de google en nuestro backend

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,4 @@
-/*jslint  esversion: 6 */
+/*jslint  esversion: 8 */
 
 var express = require('express');
 var bcrypt = require('bcryptjs');
@@ -9,8 +9,59 @@ var SEED = require('../config/config').SEED;
 var app = express();
 var Usuario = require('../models/usuario');
 
+// Google 
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+
 //===============================================================================
-// creando el login 
+// Autentificación de Google
+//===============================================================================
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload();
+    // const userid = payload.sub;
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    };
+}
+
+app.post('/google', async(request, response) => {
+
+    var token = request.body.token;
+
+    var googleUser = await verify(token)
+        .catch(err => {
+            return response.status(403).json({
+                ok: false,
+                mensaje: 'token no valido'
+            });
+        });
+
+    return response.status(200).json({
+        ok: true,
+        mensaje: 'OK!!',
+        googleUser: googleUser,
+    });
+});
+
+
+
+
+//===============================================================================
+// creando el login Autentificación Normal
 //===============================================================================
 
 app.post('/', (requets, response) => {
